74945a8fbe5dc3135d75788fa5817f8531cf6483
JuliusDanes <julius.danes.nugroho@gmail.com>
2022-08-18 18:41:07 +0700

    Fixing ArrayMerge

--- P1_ArrayMerge.java
+++ P1_ArrayMerge.java
@@ -1,34 +1,62 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
+import java.util.regex.*;
 
 public class P1_ArrayMerge {
     public static void main(String[] args) {
-        String[] A = new String[]{"kazuya", "jin", "lee"};
-        String[] B = new String[]{"kazuya", "feng"};
-        String[] tempArr = new String[A.length+B.length], M;
-        int size = 0;
+        Scanner input = new Scanner(System.in);
+        String s1, s2;
+        System.out.print("Input 1: ");
+        s1 = input.nextLine();              // Contoh input ['kazuya', 'jin', 'lee']
+        System.out.print("Input 2: ");
+        s2 = input.nextLine();              // Contoh input ['kazuya', 'feng']
 
-        for (int i=0, j=0, k=0; (i<A.length) || (j<B.length); i++) {
-            for (int x=0; x<tempArr.length; x++) {
-                if ((i < A.length)) {
-                    if (tempArr[x] == A[i]) { break; }
-                    tempArr[k] = A[i];
-                }
-                else if ((j < B.length)) {
-                    if (tempArr[x] == B[j]) { break; }
-                    tempArr[k] = B[j];
-                }
-                if ((i < A.length) || (j < B.length))
-                    k++;{
-                    size = k;
+        String[] arr1 = inputToStringArray(s1);
+        String[] arr2 = inputToStringArray(s2);
+
+        System.out.println(Arrays.toString(getArrayMerge(arr1, arr2)));
+    }
+    public static String[] inputToStringArray(String s) {
+        String[] sArr = s.replaceAll("[\\[\\]' ]+", "")
+                .split(",");
+
+        int size = sArr.length;
+        String[] arr = new String [size];
+        for(int i=0; i<size; i++) {
+            arr[i] = sArr[i];
+        }
+        return arr;
+    }
+
+    public static String[] getArrayMerge(String arr1[], String arr2[]) {
+        String[] arrMerge = new String[arr1.length+ arr2.length];
+        int i=0;
+        for (String s: arr1) {
+            arrMerge[i] = s;
+            i++;
+        }
+        for (String s: arr2) {
+            arrMerge[i] = s;
+            i++;
+        }
+        return getRemoveDuplicate(arrMerge);
+    }
+    public static String[] getRemoveDuplicate(String arr[]) {
+        List<String> al = new ArrayList<String>();
+        for (String i : arr) {
+            boolean sts = true;
+            for (String j : al) {
+                if (i.equals(j)) {
+                    sts = false;
                     break;
                 }
             }
-
-            if (i>A.length) { j++; }
+            if (sts) {
+                al.add(i);
+            }
         }
-
-        M = new String[size];
-        System.arraycopy(tempArr, 0, M, 0, size);
-        System.out.println(Arrays.toString(M));
+        return al.toArray(new String[0]);
     }
 }
